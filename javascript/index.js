b321e4d2a9391af618d3717deb9649734d6f6b4a
amineennaciri <spitlight.enn@gmail.com>
2023-02-23 13:13:35 +0100

    achieved the display of the temperature and the image that is related to the city selected. Achieved the toggle feature that convert the temperature from fahrenheit to celsius and vice-versa. achieved the display for the humidity percentage and the speed of the cit's wind.

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,3 @@
-console.log('hello world');
-////////////////////////////
 const img = document.querySelector('img');
         const btn = document.querySelector('.btn');
         const form = document.querySelector('form');
@@ -20,6 +18,7 @@ const img = document.querySelector('img');
 
         async function weatherRun(){
             const cityName = document.querySelector('#location').value;
+            getImage(cityName);
             console.log(cityName);
             const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName},${stateCode},${countryCode}&appid=${APIkey}`,{mode: 'cors'});
             const locationData = await response.json();
@@ -27,15 +26,60 @@ const img = document.querySelector('img');
             const secondResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${locationData[0].lat}&lon=${locationData[0].lon}&appid=${APIkey}`,{mode: 'cors'});
             const weatherData = await secondResponse.json();
             console.log(weatherData);
+            console.log(weatherData.main.temp);
+            // display temperature
+            const dispTemp = document.createElement('button');
+            dispTemp.innerText = `${Math.round(weatherData.main.temp-273.15)} °C`;
+            dispTemp.setAttribute('class','tempBtn')
+            document.body.appendChild(dispTemp);
+            dispTemp.addEventListener('click',toggleTemperature);
+            // display humidity
+            const disphumidity = document.createElement('button');
+            disphumidity.innerText = `Humidity ${Math.round(weatherData.main.humidity)} %`;
+            disphumidity.setAttribute('class','humidityBtn')
+            document.body.appendChild(disphumidity);
+            // display wind speed
+            const dispwindSpeed = document.createElement('button');
+            dispwindSpeed.innerText = `Wind speed equal ${Math.round(weatherData.wind.speed)} m/s`;
+            dispwindSpeed.setAttribute('class','windspdBtn')
+            document.body.appendChild(dispwindSpeed);
+            
         }
         //weatherRun();
-/* 
-
-        async function runFirst(){
-            const firstResponse = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=ZHZdlYgH9XPNSFgE0ORImXO5Tw42KpXs&s=cats', {mode: 'cors'});
+        //const searchItem = "new york";//"cats";
+        async function getImage(searchItem){
+            const firstResponse = await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=ZHZdlYgH9XPNSFgE0ORImXO5Tw42KpXs&s=${searchItem}`, {mode: 'cors'});
             const firstCatData = await firstResponse.json();
             img.src = firstCatData.data.images.original.url;
         }
-        runFirst(); */
-// toggle displaying the data in Fahrenheit or Celsius.
-// You could even use the Giphy API to find appropriate weather-related gifs and display them
+        function toggleTemperature(){
+            const inputData = document.querySelector('.tempBtn').innerHTML;
+            if(inputData.split(' ')[1] ==='°C'){
+                const tempCelsius = document.querySelector('.tempBtn').innerHTML;
+                console.log(tempCelsius);
+                let tempFahrenheit = (tempCelsius.split(' ')[0]*(9/5))+32;
+                console.log(tempFahrenheit);
+                tempFahrenheit = Math.round(tempFahrenheit);
+                console.log(tempFahrenheit);
+                document.querySelector('.tempBtn').innerText = `${tempFahrenheit} °F`; 
+            }else if(inputData.split(' ')[1] ==='°F'){
+                const tempFahrenheit = document.querySelector('.tempBtn').innerHTML;
+                console.log(tempFahrenheit);
+                const numFahrenheit = tempFahrenheit.split(' ')[0];
+                console.log(numFahrenheit);
+                const constantNum = 32;
+                console.log(numFahrenheit - constantNum)
+                let tempCelsius = (numFahrenheit - constantNum)*(5/9);
+                console.log(tempCelsius);
+                tempCelsius = Math.round(tempCelsius);
+                console.log(tempCelsius);
+                document.querySelector('.tempBtn').innerText = `${tempCelsius} °C`; 
+            }else{
+                console.log('error in the temperature!')
+            }
+        }
+        //runFirst();
+// DONE -- toggle displaying the data in Fahrenheit or Celsius.
+// DONEHumidity
+// DONE wind speed
+//  DONE --> You could even use the Giphy API to find appropriate weather-related gifs and display them
